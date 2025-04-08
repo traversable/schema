bcf4b10a3abef855c24b6397b5394429ea8a07a3
Andrew Jarrett <ahrjarrett@gmail.com>
2025-04-08 17:32:06 -0500

    fix(build): uses node:path to import README template

--- bin/assets/index.ts
+++ bin/assets/index.ts
@@ -1,5 +1,10 @@
 import * as fs from 'node:fs'
+import * as path from 'node:path'
+
+const PATH = {
+  README: path.join(path.resolve(), 'bin', 'assets', '_README.md')
+} as const
 
 export { default as template } from "./_package.json"
 
-export let getReadmeTemplate = () => fs.readFileSync('./_README.md').toString('utf8')
+export let getReadmeTemplate = () => fs.readFileSync(PATH.README).toString('utf8')
