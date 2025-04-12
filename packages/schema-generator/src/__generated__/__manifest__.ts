3ca3de1652bfcb19187d0fb20c8d69bd15a6ac97
Andrew Jarrett <ahrjarrett@gmail.com>
2025-04-12 11:49:54 -0500

    chore(generator): commits generated file

--- packages/schema-generator/src/__generated__/__manifest__.ts
+++ packages/schema-generator/src/__generated__/__manifest__.ts
@@ -38,7 +38,7 @@ export default {
     "clean": "pnpm run \"/^clean:.*/\"",
     "clean:build": "rm -rf .tsbuildinfo dist build",
     "clean:deps": "rm -rf node_modules",
-    "postinstall": "./src/cli.ts",
+    "postinstall": "pnpm dlx tsx ./src/cli.ts",
     "test": "vitest"
   },
   "peerDependencies": {
