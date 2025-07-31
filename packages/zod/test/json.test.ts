d72f59d16377f6b9601360b701e4e16742460cfb
Andrew Jarrett <ahrjarrett@gmail.com>
2025-07-31 05:54:15 -0500

    test(zod): adds tests for `zx.fromConstant.writeable`, `zx.fromJson.writeable`

--- packages/zod/test/json.test.ts
+++ packages/zod/test/json.test.ts
@@ -97,6 +97,101 @@ vi.describe('〖⛳️〗‹‹‹ ❲@traversable/zod❳: zx.fromConstant', () 
   })
 })
 
+vi.describe('〖⛳️〗‹‹‹ ❲@traversable/zod❳: zx.fromConstant.writeable', () => {
+  vi.test('〖⛳️〗› ❲zx.fromConstant.writeable❳: undefined', () => {
+    vi.expect(
+      zx.fromConstant.writeable(undefined)
+    ).toMatchInlineSnapshot
+      (`"z.literal(undefined)"`)
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromConstant.writeable❳: null', () => {
+    vi.expect(
+      zx.fromConstant.writeable(null)
+    ).toMatchInlineSnapshot
+      (`"z.literal(null)"`)
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromConstant.writeable❳: true', () => {
+    vi.expect(
+      zx.fromConstant.writeable(true)
+    ).toMatchInlineSnapshot
+      (`"z.literal(true)"`)
+
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromConstant.writeable❳: false', () => {
+    vi.expect(
+      zx.fromConstant.writeable(false)
+    ).toMatchInlineSnapshot
+      (`"z.literal(false)"`)
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromConstant.writeable❳: number', () => {
+    vi.expect(
+      zx.fromConstant.writeable(-0)
+    ).toMatchInlineSnapshot
+      (`"z.literal(0)"`)
+    vi.expect(
+      zx.fromConstant.writeable(0)
+    ).toMatchInlineSnapshot
+      (`"z.literal(0)"`)
+    vi.expect(
+      zx.fromConstant.writeable(1.1)
+    ).toMatchInlineSnapshot
+      (`"z.literal(1.1)"`)
+    vi.expect(
+      zx.fromConstant.writeable(1e+29)
+    ).toMatchInlineSnapshot
+      (`"z.literal(1e+29)"`)
+    vi.expect(
+      zx.fromConstant.writeable(1e-29)
+    ).toMatchInlineSnapshot
+      (`"z.literal(1e-29)"`)
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromConstant.writeable❳: string', () => {
+    vi.expect(
+      zx.fromConstant.writeable('')
+    ).toMatchInlineSnapshot
+      (`"z.literal("")"`)
+    vi.expect(
+      zx.fromConstant.writeable('abc')
+    ).toMatchInlineSnapshot
+      (`"z.literal("abc")"`)
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromConstant.writeable❳: array', () => {
+    vi.expect(
+      zx.fromConstant.writeable([])
+    ).toMatchInlineSnapshot
+      (`"z.tuple([])"`)
+    vi.expect(
+      zx.fromConstant.writeable([0])
+    ).toMatchInlineSnapshot
+      (`"z.tuple([z.literal(0)])"`)
+    vi.expect(
+      zx.fromConstant.writeable([0, [1, [2]]])
+    ).toMatchInlineSnapshot
+      (`"z.tuple([z.literal(0),z.tuple([z.literal(1),z.tuple([z.literal(2)])])])"`)
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromConstant.writeable❳: object', () => {
+    vi.expect(
+      zx.fromConstant.writeable({})
+    ).toMatchInlineSnapshot
+      (`"z.object({}).catchall(z.never())"`)
+    vi.expect(
+      zx.fromConstant.writeable({ '': '' })
+    ).toMatchInlineSnapshot
+      (`"z.object({"":z.literal("")}).catchall(z.never())"`)
+    vi.expect(
+      zx.fromConstant.writeable({ 'a': { b: 'B', c: { d: ['D', { e: 'E' }] } } })
+    ).toMatchInlineSnapshot
+      (`"z.object({a:z.object({b:z.literal("B"),c:z.object({d:z.tuple([z.literal("D"),z.object({e:z.literal("E")}).catchall(z.never())])}).catchall(z.never())}).catchall(z.never())}).catchall(z.never())"`)
+  })
+})
+
 vi.describe('〖⛳️〗‹‹‹ ❲@traversable/zod❳: zx.fromJson', () => {
   vi.test('〖⛳️〗› ❲zx.fromJson❳: undefined', () => {
     vi.expect(zx.toString(
@@ -187,6 +282,100 @@ vi.describe('〖⛳️〗‹‹‹ ❲@traversable/zod❳: zx.fromJson', () => {
     vi.expect(zx.toString(
       zx.fromJson({ 'a': { b: 'B', c: { d: ['D', { e: 'E' }] } } })
     )).toMatchInlineSnapshot
+      (`"z.object({a:z.object({b:z.string(),c:z.object({d:z.array(z.union([z.string(),z.object({e:z.string()}).catchall(z.unknown())]))}).catchall(z.unknown())}).catchall(z.unknown())}).catchall(z.unknown())"`)
+  })
+})
+
+vi.describe('〖⛳️〗‹‹‹ ❲@traversable/zod❳: zx.fromJson.writeable', () => {
+  vi.test('〖⛳️〗› ❲zx.fromJson.writeable❳: undefined', () => {
+    vi.expect(
+      zx.fromJson.writeable(undefined)
+    ).toMatchInlineSnapshot
+      (`"z.null()"`)
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromJson.writeable❳: null', () => {
+    vi.expect(
+      zx.fromJson.writeable(null)
+    ).toMatchInlineSnapshot
+      (`"z.null()"`)
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromJson.writeable❳: true', () => {
+    vi.expect(
+      zx.fromJson.writeable(true)
+    ).toMatchInlineSnapshot
+      (`"z.boolean()"`)
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromJson.writeable❳: false', () => {
+    vi.expect(
+      zx.fromJson.writeable(false)
+    ).toMatchInlineSnapshot
+      (`"z.boolean()"`)
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromJson.writeable❳: number', () => {
+    vi.expect(
+      zx.fromJson.writeable(-0)
+    ).toMatchInlineSnapshot
+      (`"z.number()"`)
+    vi.expect(
+      zx.fromJson.writeable(0)
+    ).toMatchInlineSnapshot
+      (`"z.number()"`)
+    vi.expect(
+      zx.fromJson.writeable(1.1)
+    ).toMatchInlineSnapshot
+      (`"z.number()"`)
+    vi.expect(
+      zx.fromJson.writeable(1e+29)
+    ).toMatchInlineSnapshot
+      (`"z.number()"`)
+    vi.expect(
+      zx.fromJson.writeable(1e-29)
+    ).toMatchInlineSnapshot
+      (`"z.number()"`)
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromJson.writeable❳: string', () => {
+    vi.expect(
+      zx.fromJson.writeable('')
+    ).toMatchInlineSnapshot
+      (`"z.string()"`)
+    vi.expect(
+      zx.fromJson.writeable('abc')
+    ).toMatchInlineSnapshot
+      (`"z.string()"`)
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromJson.writeable❳: array', () => {
+    vi.expect(
+      zx.fromJson.writeable([])
+    ).toMatchInlineSnapshot
+      (`"z.array(z.unknown())"`)
+    vi.expect(
+      zx.fromJson.writeable([0])
+    ).toMatchInlineSnapshot
+      (`"z.array(z.number())"`)
+    vi.expect(
+      zx.fromJson.writeable([0, [1, [2]]])
+    ).toMatchInlineSnapshot
+      (`"z.array(z.union([z.number(),z.array(z.union([z.number(),z.array(z.number())]))]))"`)
+  })
+
+  vi.test('〖⛳️〗› ❲zx.fromJson.writeable❳: object', () => {
+    vi.expect(
+      zx.fromJson.writeable({})
+    ).toMatchInlineSnapshot
+      (`"z.object({}).catchall(z.unknown())"`)
+    vi.expect(
+      zx.fromJson.writeable({ '': '' })
+    ).toMatchInlineSnapshot
+      (`"z.object({"":z.string()}).catchall(z.unknown())"`)
+    vi.expect(
+      zx.fromJson.writeable({ 'a': { b: 'B', c: { d: ['D', { e: 'E' }] } } })
+    ).toMatchInlineSnapshot
       (`"z.object({a:z.object({b:z.string(),c:z.object({d:z.array(z.union([z.string(),z.object({e:z.string()}).catchall(z.unknown())]))}).catchall(z.unknown())}).catchall(z.unknown())}).catchall(z.unknown())"`)
   })
 })
