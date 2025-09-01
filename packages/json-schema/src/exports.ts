902b974e03bd0ac5104d1830ee1f7073baa30b62
Andrew Jarrett <ahrjarrett@gmail.com>
2025-09-01 14:14:12 -0500

    fix(json-schema): re-exports types from `@traversable/json-schema-types` package

--- packages/json-schema/src/exports.ts
+++ packages/json-schema/src/exports.ts
@@ -1,5 +1,6 @@
-export * from '@traversable/json-schema-types'
+export type * from '@traversable/json-schema-types/types'
 export { fold } from '@traversable/json-schema-types'
+
 export { VERSION } from './version.js'
 export { deepEqual } from './deep-equal.js'
 export { deepClone } from './deep-clone.js'
