f48fceb04dadd98f15864761eccb8d54e0e34c33
Andrew Jarrett <ahrjarrett@gmail.com>
2025-08-31 23:10:44 -0500

    feat(zod-test): implements `unbounded` option for `SeedGenerator`

---
+++ packages/zod-test/test/generator.test.ts
@@ -0,0 +1,115 @@
+import * as vi from 'vitest'
+import { zxTest } from '@traversable/zod-test'
+import * as fc from 'fast-check'
+
+vi.describe('〖️⛳️〗‹‹‹ ❲@traversable/zod-test❳', () => {
+  vi.test('〖️⛳️〗› ❲zxTest.SeedGenerator❳: int', () => {
+    vi.expect.soft(
+      fc.sample(
+        zxTest.SeedGenerator({
+          include: ['int'],
+          int: { unbounded: true }
+        })['int'],
+        1,
+      )[0]
+    ).toMatchInlineSnapshot
+      (`
+      [
+        100,
+        [
+          null,
+          null,
+          null,
+        ],
+      ]
+    `)
+  })
+
+  vi.test('〖️⛳️〗› ❲zxTest.SeedGenerator❳: bigint', () => {
+    vi.expect.soft(
+      fc.sample(
+        zxTest.SeedGenerator({
+          include: ['bigint'],
+          bigint: { unbounded: true }
+        })['bigint'],
+        1,
+      )[0]
+    ).toMatchInlineSnapshot
+      (`
+      [
+        150,
+        [
+          null,
+          null,
+          null,
+        ],
+      ]
+    `)
+  })
+
+  vi.test('〖️⛳️〗› ❲zxTest.SeedGenerator❳: number', () => {
+    vi.expect.soft(
+      fc.sample(
+        zxTest.SeedGenerator({
+          include: ['number'],
+          number: { unbounded: true }
+        })['number'],
+        1,
+      )[0]
+    ).toMatchInlineSnapshot
+      (`
+      [
+        200,
+        [
+          null,
+          null,
+          null,
+          false,
+          false,
+        ],
+      ]
+    `)
+  })
+
+  vi.test('〖️⛳️〗› ❲zxTest.SeedGenerator❳: string', () => {
+    vi.expect.soft(
+      fc.sample(
+        zxTest.SeedGenerator({
+          include: ['string'],
+          string: { unbounded: true }
+        })['string'],
+        1,
+      )[0]
+    ).toMatchInlineSnapshot
+      (`
+      [
+        250,
+        [
+          null,
+          null,
+        ],
+      ]
+    `)
+  })
+
+  vi.test('〖️⛳️〗› ❲zxTest.SeedGenerator❳: array', () => {
+    vi.expect.soft(
+      fc.sample(
+        zxTest.SeedGenerator({
+          include: ['array'],
+          array: { unbounded: true }
+        })['array'],
+        1,
+      )[0]
+    ).toMatchInlineSnapshot
+      (`
+      [
+        1000,
+        [
+          null,
+          null,
+        ],
+      ]
+    `)
+  })
+})
