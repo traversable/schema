4b9e656e3aa611551a084b2f47b27dd61d891ea3
Andrew Jarrett <ahrjarrett@gmail.com>
2025-09-01 14:25:12 -0500

    fix(json-schema): exports terms by name

--- packages/json-schema/src/exports.ts
+++ packages/json-schema/src/exports.ts
@@ -1,5 +1,44 @@
-export type * from '@traversable/json-schema-types/types'
-export { fold } from '@traversable/json-schema-types'
+export type {
+  Array,
+  Boolean,
+  Const,
+  Enum,
+  F,
+  Integer,
+  Intersection,
+  Never,
+  Null,
+  Nullary,
+  Number,
+  Object,
+  Record,
+  String,
+  Tuple,
+  Union,
+  Unknown
+} from '@traversable/json-schema-types'
+export {
+  fold,
+  Functor,
+  isArray,
+  isBoolean,
+  isConst,
+  isEnum,
+  isInteger,
+  isIntersection,
+  isNever,
+  isNull,
+  isNullary,
+  isNumber,
+  isObject,
+  isRecord,
+  isString,
+  isTuple,
+  isUnion,
+  isUnknown,
+  toType,
+  TypeName,
+} from '@traversable/json-schema-types'
 
 export { VERSION } from './version.js'
 export { deepEqual } from './deep-equal.js'
