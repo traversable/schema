991aaa2250eac1be666bbe9406836bba71e8f4e1
Andrew Jarrett <ahrjarrett@gmail.com>
2025-09-01 13:40:58 -0500

    fix(json-schema,zod): fixes `JsonSchema.fold` type inference

--- packages/json-schema/src/exports.ts
+++ packages/json-schema/src/exports.ts
@@ -1,4 +1,5 @@
 export * from '@traversable/json-schema-types'
+export { fold } from '@traversable/json-schema-types'
 export { VERSION } from './version.js'
 export { deepEqual } from './deep-equal.js'
 export { deepClone } from './deep-clone.js'
--- packages/json-schema/src/index.ts
+++ packages/json-schema/src/index.ts
@@ -1,3 +1,6 @@
+export * from '@traversable/json-schema-types'
+
+export { VERSION } from './version.js'
 export * from './exports.js'
 export type JsonSchema<T = never> = import('@traversable/json-schema-types').JsonSchema<T>
 export * as JsonSchema from './exports.js'
