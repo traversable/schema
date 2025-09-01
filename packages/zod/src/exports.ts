c004e4196ef93d280f780d2780edfd15e27fb369
Andrew Jarrett <ahrjarrett@gmail.com>
2025-09-01 11:31:49 -0500

    fix(zod): exports `isOptional` and `isOptionalDeep` from `zx` namespace

--- packages/zod/src/exports.ts
+++ packages/zod/src/exports.ts
@@ -1,3 +1,6 @@
+export { isOptional, isOptionalDeep } from '@traversable/zod-types'
+
+export { RAISE_ISSUE_URL, VERSION, ZOD_CHANGELOG, ZOD_VERSION } from './version.js'
 export { check } from './check.js'
 export { deepClone } from './deep-clone.js'
 export { deepEqual } from './deep-equal.js'
